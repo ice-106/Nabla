8edc1b1cdb940981d5ac33beed186181c60a3ba9
Imeany12 <oat.ueaangkun.inyaem@gmail.com>
2025-12-22 00:55:35 +0700

    chore: clean dynamic site_pks

--- scripts/prepare/OSX.sh
+++ scripts/prepare/OSX.sh
@@ -2,7 +2,7 @@
 set -x
 
 echo "Start preparing OSX environment"
-conda create -n osx python=3.9
+conda create -n osx python=3.9 -y
 
 conda run -n osx pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
 
