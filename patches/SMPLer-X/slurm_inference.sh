9cfa98ed4210a7b3070ca94c199eea7902625a6c
Imeany12 <oat.ueaangkun.inyaem@gmail.com>
2026-01-18 16:57:33 +0700

    chore: clean up smplerx

--- patches/SMPLer-X/slurm_inference.sh
+++ patches/SMPLer-X/slurm_inference.sh
@@ -20,28 +20,31 @@ cd utils/extraction/SMPLer-X/main
 IMG_PATH=../demo/images/${INPUT_VIDEO}
 SAVE_DIR=../demo/results/${INPUT_VIDEO}
 
-# video to images
+# video (or png frames) to images
 mkdir -p $IMG_PATH
 mkdir -p $SAVE_DIR
-ffmpeg -n -i ../demo/videos/${INPUT_VIDEO}.${FORMAT} -f image2 -vf fps=${FPS}/1 -q:v 0 ../demo/images/${INPUT_VIDEO}/%06d.jpg < /dev/null 
 
-end_count=$(find "$IMG_PATH" -type f | wc -l)
+end_count=$(find "$IMG_PATH" -type f -name "*.$FORMAT" | wc -l)
 echo $end_count
 
 # inference
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-conda run -n smplerx python inference.py \
---num_gpus ${GPUS_PER_NODE} \
---exp_name output/demo_${JOB_NAME} \
---pretrained_model ${CKPT} \
---agora_benchmark agora_model \
---img_path ${IMG_PATH} \
---start 1 \
---end $end_count \
---output_folder ${SAVE_DIR} \
---show_verts \
---show_bbox \
---save_mesh \
-# --multi_person \
-# --iou_thr 0.2 \
-# --bbox_thr 20
+    conda run -n smplerx python inference.py \
+    --num_gpus ${GPUS_PER_NODE} \
+    --exp_name output/demo_${JOB_NAME} \
+    --pretrained_model ${CKPT} \
+    --agora_benchmark agora_model \
+    --img_path ${IMG_PATH} \
+    --start 1 \
+    --end  $end_count \
+    --output_folder ${SAVE_DIR} \
+    --show_verts \
+    --show_bbox \
+    --save_mesh \
+    # --multi_person \
+    # --iou_thr 0.2 \
+    # --bbox_thr 20
+
+
+# images to video
+# ffmpeg -y -f image2 -r ${FPS} -i ${SAVE_DIR}/img/%06d.jpg -vcodec mjpeg -qscale 0 -pix_fmt yuv420p ../demo/results/${INPUT_VIDEO}.mp4
